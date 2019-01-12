3c2aac9cae7fd838e28306197e76d80a9c74ecae
Andrew Menrije <andmenrije@gmail.com>
2019-01-12 01:47:44 -0800

    Initial checkin of code. Added floor, obstacles, and player with initial movement capability.

---
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate movement velocity as 3D vector
+        float xMovement = Input.GetAxisRaw("Horizontal"); 
+        float zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMovement;
+        Vector3 moveVertical = transform.forward * zMovement;
+
+        // Final movement vecctor
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+    }
+}
---
+++ Maze/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
